c472f696c8ae55c31c6fb1a24d563774e1cb2c24
Didigwu Nnaemeka Christian <42030004+NnaDid@users.noreply.github.com>
2019-09-17 14:26:57 +0100

    Create 404.php

---
+++ vierws/404.php
@@ -0,0 +1,11 @@
+	<!-- Partners Logo -->
+	<div class="row mb-4 mt-4 p-4" style="border:1px solid #70AD47; border-right: 0;border-left: 0;">
+			
+	<div class ="row partners">
+		  <div class="col-lg-12">
+                   PAGE NOT FOUND 
+                   <hr/>
+                   <h2 align ="center"> 404</h2>
+          </div>
+        </div>	
+	</div>
