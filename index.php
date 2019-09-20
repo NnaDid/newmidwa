e11a363d3760d3607da5d210820823e3e5bae3ed
Didigwu Nnaemeka Christian <42030004+NnaDid@users.noreply.github.com>
2019-09-20 10:54:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,12 +11,35 @@
 	<link rel ="stylesheet" 	href ="css/bootstrap.min.css" />
 	<link rel ="stylesheet" 	href ="css/main.css" />
 	<link rel ="shortcut icon"  href ="images/logo.png"/>
-
+	<style>
+		ul,.menulist{
+			list-style-type:none;
+			margin-left:0px;
+		}
+		ul.submenu,ul.submenu-2,ul.submenu-3{
+			display:none;
+			left:190px;
+			transition: 0.6s;
+			background-color:#f1f1f1;
+			position:absolute;
+			top:calc(0 - 1px);
+			min-width: 230px;
+			box-shadow: 0px 8px 16px 0px rgba(200,0,20,0.2);
+			padding:4px;
+			z-index:200000;
+		}
+		.menulist li:hover ul.submenu{
+			display:block;
+		}
+		.submenu li:hover ul.submenu-2{
+			display:block;
+		}
+	</style>
 <script src="https://kit.fontawesome.com/a276793cb3.js"></script>
    </head>
    
-    <body>
-     <div class ="bg-light container main">
+    <body class ="app-bg">
+     <div class =" app-bg container main">
 	
 	      	<nav style ="border-bottom:1px solid #70AD47; text-align:right;">
 				<a class="btn btn-outline-success btn-sm" href="?pg=signup">Register</a>
@@ -27,10 +50,10 @@
 			</nav>
 
 
-		<nav class="navbar navbar-expand-lg navbar-light bg-light" style ="border-bottom:2px solid #70AD47;">
+		<nav class="navbar navbar-expand-lg navbar-light app-bg" style ="border-bottom:2px solid #70AD47;">
 		<div class="collapse navbar-collapse col-md-12" id="navbarTogglerDemo01">
-			<a class="navbar-brand" href="#">
-			<img src="images/logo.png" class ="img-responsive" width ="150" height ="150" style ="border-radius:4em;"/>
+			<a class="navbar-brand" href="index.php">
+			<img src="images/logo.png" class ="img-responsive" width ="130" height ="130" style ="border-radius:8em;"/>
 			<span class ="bold hidden-xs-down">Migration Dialogue For West Africa</span>
 			</a>
 			<ul class="navbar-nav ml-auto" >
@@ -50,7 +73,7 @@
 
 
 	
-		<nav class="navbar navbar-expand-lg navbar-light bg-light mb-2" style ="border-bottom:2px solid #70AD47;">
+		<nav class="navbar navbar-expand-lg navbar-light app-bg mb-2" style ="border-bottom:2px solid #70AD47;">
 		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		  </button>
@@ -59,61 +82,91 @@
 			  <li class="nav-item dropdown">
 				<a class="nav-link">About Us <i class ="fa fa-caret-down"></i></a>
 				  <div class="dropdown-content">
-				  <a class="" href="#">Brief History of MIDWA</a>
-				  <a class="" href="#">Our Objectives</a>
-				  <a class="" href="#">What We are doing</a>
-				  <a class="" href="#">Governance Structure</a>
-				  <a class="" href="#">Our Achievements</a>
-				  <a class="" href="#">MIDWA Team</a>
-				  <a class="" href="#">Partners</a>
-
+					<ul class ="menulist" style ="margin:0px;">
+						<li class="nav-item"><a class="" href="#">Brief History Of MIDWA</a></li>
+						<li class="nav-item"><a class="" href="#">Mandate</a></li>
+						<li class="nav-item"><a class="" href="#">Our Objectives</a></li>
+						<li class="nav-item"><a class="" href="#">What We Are Doing</a></li>
+						<li class="nav-item"><a class="" href="#">MIDWA Secretariat</a>
+							<ul class="submenu">
+								<li class="nav-item"><a class="" href="?pg=media&media=photos">Governance Structure</a></li>
+								<li class="nav-item"><a class="" href="?pg=media&media=videos">Organogram</a></li> 
+							</ul>
+						</li>
+						<li class="nav-item"><a class="" href="#">Our Achievements</a></li>
+						<li class="nav-item"></li><a class="" href="#">Partners</a></li>
+					</ul>
 				  </div>
 			 </li>
 			  <li class="nav-item dropdown">
 				<a class="nav-link" href="#">Thematic areas 
 				<i class ="fa fa-caret-down"></i></a>
 				<div class="dropdown-content">
+				  <a class="" href="#">Border Management</a>
 				  <a class="" href="#">Labour Migration</a>
 				  <a class="" href="#">Migration Data</a>
-				  <a class="" href="#">Border Management</a>
+				  <a class="" href="#">Mixed Migration</a>
 				  <a class="" href="#">Return and Re-integration</a>
-				  <a class="" href="#">Mixed Migration/Protection</a>
+
 				  </div>
 			  </li>
 
+			  <li class="nav-item dropdown">
+				<a class="nav-link" href="#">Strategy <i class ="fa fa-caret-down"></i></a>
+				  <div class="dropdown-content">
+				  <ul class ="menulist">
+				  <li class="nav-item"><a class="" href="?pg=dialogue">Dialogue</a>
+				         <ul class="submenu">
+									<li class="nav-item"><a class="" href="?pg=meetings&meetings=global">Expert  Meetings</a></li> 
+									<li class="nav-item"><a class="" href="?pg=meetings&meetings=national">Ministerial Meetings</a></li>
+									<li class="nav-item"><a class="" href="?pg=meetings&meetings=secretariate">Secretariat Meetings</a></li> 
+							</ul>
+				</li>
+				  <li class="nav-item"><a class="" href="?pg=regional_meeting">Facilitations</a></li>
+				 <!-- <li class="nav-item"><a class="" href="?pg=contenental_meeting">Meetings</a>
+				     <ul class="submenu">
+						<li class="nav-item"><a class="" href="?pg=mettings&meetings=continental">Continental Meetings</a>
 
+						</li>
+						<li class="nav-item"><a class="" href="?pg=meetings&meetings=global">Global Meetings</a></li> 
+						<li class="nav-item"><a class="" href="?pg=meetings&meetings=national">National Meetings</a></li>
+						<li class="nav-item"><a class="" href="?pg=meetings&meetings=regional">Regional Meetings</a></li>  
+			
+					</ul>
+				  </li>-->
+				  <li class="nav-item"><a class="" href="#">Policy Formulation</a></li>
+				 </ul>
+				  </div>
+
+			  </li> 
 
 			  <li class="nav-item dropdown">
 				<a class="nav-link" href="#">Resources<i class ="fa fa-caret-down"></i></a>
-				<div class="dropdown-content">
-				  <a class="dropdown" href="#">Questionaires </i></a>
-				  <a class="dropdown" href="#">ECOMIS</a>
-				  </div>
+					<div class="dropdown-content">	
+					<a class="dropdown" href="?pg=report">Reports</a>
+					<a class="" href="?pg=studies">Studies</a>
+					<a class="extMenu" href="?pg=publications">Publications</a>
+					</div>
 			  </li>
-			  <li class="nav-item dropdown">
-				<a class="nav-link" href="#">Meetings <i class ="fa fa-caret-down"></i></a>
-				  <div class="dropdown-content">
-				  <a class="" href="#">National Meetings</a>
-				  <a class="" href="?pg=regional_meeting">Regional Meetings</a>
-				  <a class="" href="#">Secretariate Meetings</a>
-				  <a class="" href="?pg=contenental_meeting">Continental Meetings</a>
-				  <a class="" href="?pg=global_meeting">Global Meetings</a>
-				  </div>
-			  </li> 
-			  <li class="nav-item dropdown">
-				<a class="nav-link" href="#">Documentations<i class ="fa fa-caret-down"></i></a>
-				<div class="dropdown-content">
-				  <a class="extMenu" href="?pg=media">Media</a>
-				     <div class ="megaTreeMenu">
-							<a class="" href="#">Video Gallery</a>
-							<a class="" href="#">Photo Galery</a>
-					 </div>
-				  <a class="" href="?pg=publications">Publications</a>
-				  <a class="" href="?pg=articles">Articles</a>
-				  <a class="" href="?pg=reports">Reports</a>
-				  <a class="" href="?pg=newsletters">Newsletters</a>
 
-				  </div>
+			  <li class="nav-item dropdown">
+				<a class="nav-link" href="#"> Communications <i class ="fa fa-caret-down"></i></a>
+				<div class="dropdown-content">
+				  <ul class ="menulist">
+				  <li class="nav-item"><a class="" href="?pg=articles">Articles</a>
+				  <li class="nav-item"><a class="" href="?pg=media">Media</a>
+				    <ul class="submenu">
+						<li class="nav-item"><a class="" href="?pg=media&media=photos">Photo  Gallery</a></li>
+						<li class="nav-item"><a class="" href="?pg=media&media=videos">Videos</a></li> 
+					</ul>
+				  </li>
+				  <li class=""><a class="" href="?pg=newsletters">Newsletters</a>
+				</li>
+				  <li class="nav-item"><a class="" href="?pg=press_release">Press Release</a></li>
+				  <li class="nav-item"><a class="" href="?pg=visibility">Visibility</a></li>
+				 
+				</ul>
+				</div>
 			  </li>
 			  <li class="nav-item dropdown">
 				<a class="nav-link" href="?pg=contactus">Contact Us</a>
@@ -202,6 +255,18 @@
 <script>
   // this script is for switching signUo and Sign In forms
   $(document).ready(()=>{
+	  // Handle Selection of institutions during signup
+	  let institution = $(".institution");
+	  let whenOthers  = $(".when-others");
+	  $(document).on("change",institution,function(evt){
+        let insValue = evt.target.value;
+		if(insValue==="Others"){
+			whenOthers.removeAttr("hidden");
+		}else{
+			whenOthers.attr("hidden",true);
+		}
+	  });
+
 	   $(".loginBtn").click((evt)=>{
 		   evt.preventDefault();
 		   $(".signUpModal").fadeOut(1000);
